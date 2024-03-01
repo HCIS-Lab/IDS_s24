6c092e3dda5a866e6477549a5b8053323de2b539
Reezzz702 <changhowad@gmail.com>
2024-03-01 17:19:52 +0800

    update carla 10 url

--- HW0/setup_carla_10.sh
+++ HW0/setup_carla_10.sh
@@ -3,8 +3,8 @@
 # Download and install CARLA
 mkdir carla_10
 cd carla_10
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.10.1.tar.gz
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/AdditionalMaps_0.9.10.1.tar.gz
+wget https://carla-releases.s3.us-east-005.backblazeb2.com/Linux/CARLA_0.9.10.1.tar.gz  --no-check-certificate
+wget https://carla-releases.s3.us-east-005.backblazeb2.com/Linux/AdditionalMaps_0.9.10.1.tar.gz --no-check-certificate
 tar -xf CARLA_0.9.10.1.tar.gz
 tar -xf AdditionalMaps_0.9.10.1.tar.gz
 rm CARLA_0.9.10.1.tar.gz
